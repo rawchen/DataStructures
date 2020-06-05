821353043d479d713d0a3bb1eadcff3d0d6786c9
yoyling <2221999792@qq.com>
2020-06-05 23:49:55 +0800

    Array

---
+++ array/Test2.java
@@ -0,0 +1,20 @@
+package array;
+
+// Java program to demonstrate  
+// return of array from method 
+  
+public class Test2 {     
+    // Driver method 
+    public static void main(String args[]) { 
+        int arr[] = m1(); 
+          
+        for (int i = 0; i < arr.length; i++) 
+            System.out.print(arr[i]+" "); 
+      
+    } 
+  
+    public static int[] m1() { 
+        // returning  array 
+        return new int[]{1,2,3}; 
+    } 
+}
