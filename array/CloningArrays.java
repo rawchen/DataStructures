02aaf83dc0f1502673d4e3df9ab6affe141dce8d
yoyling <2221999792@qq.com>
2020-06-06 22:00:09 +0800

    增加克隆多维数组类

--- array/CloningArrays.java
+++ array/CloningArrays.java
@@ -1,4 +1,20 @@
 package array;
 
+
+// Java program to demonstrate
+// cloning of one-dimensional arrays
 public class CloningArrays {
+    public static void main(String[] args) {
+        int intArray[] = {1,2,3};
+
+        int cloneArray[] = intArray.clone();
+
+        // will print false as deep copy is created
+        // for one-dimensional array
+        System.out.println(intArray == cloneArray);
+
+        for (int i = 0; i < cloneArray.length; i++) {
+            System.out.print(cloneArray[i]+" ");
+        }
+    }
 }
