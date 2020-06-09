f18bd94027c16c9e48ff9f60df1bfca80b938ab5
yoyling <2221999792@qq.com>
2020-06-09 22:35:29 +0800

    单向链表成员方法

--- linkList/LinkList.java
+++ linkList/LinkList.java
@@ -20,7 +20,35 @@ public class LinkList<T> {
     }
 
     public LinkList() {
-
+        //初始化头结点
+        this.head = new Node(null,null);
+        //初始化元素个数
+        this.N = 0;
     }
 
+    //清空链表
+    public void clear() {
+        head.next = null;
+        N = 0;
+    }
+
+    //获取链表的长度
+    public int length() {
+        return N;
+    }
+
+    //判断链表是否为空
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    //获取指定位置i索引处的元素
+    public T get(int i) {
+        //通过循环从头结点开始往后找，依次找i次
+        Node n = head.next;
+        for (int index = 0; index < i; index++) {
+            n = n.next;
+        }
+        return n.item;
+    }
 }
---
+++ test/LinkListTest.java
@@ -0,0 +1,7 @@
+package test;
+
+import linkList.LinkList;
+
+public class LinkListTest {
+    LinkList<String> linkList = new LinkList<>();
+}
