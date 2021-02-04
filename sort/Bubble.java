d1a9b59f25734616376ceeab6963e45f267c5791
yoyling <2221999792@qq.com>
2021-02-04 12:49:24 +0800

    冒泡排序

--- sort/Bubble.java
+++ sort/Bubble.java
@@ -1,9 +1,11 @@
 package sort;
 
 public class Bubble {
-	/*
-	   对数组a中的元素进行排序
-	*/
+	
+	/**
+	 * 对数组a中的元素进行排序
+	 * @param a
+	 */
 	public static void sort(Comparable[] a) {
 		for (int i = a.length - 1; i > 0; i--) {
 			for (int j = 0; j < i; j++) {
@@ -16,15 +18,21 @@ public class Bubble {
 		}
 	}
 
-	/*
-		比较v元素是否大于w元素
+	/**
+	 * 比较v元素是否大于w元素
+	 * @param v
+	 * @param w
+	 * @return
 	 */
 	private static boolean greater(Comparable v, Comparable w) {
 		return v.compareTo(w) > 0;
 	}
 
-	/*
-	数组元素i和j交换位置
+	/**
+	 * 数组元素i和j交换位置
+	 * @param a
+	 * @param i
+	 * @param j
 	 */
 	private static void exch(Comparable[] a, int i, int j) {
 		Comparable temp;
--- test/BubbleTest.java
+++ test/BubbleTest.java
@@ -4,6 +4,9 @@ import sort.Bubble;
 
 import java.util.Arrays;
 
+/**
+ * 冒泡排序测试
+ */
 public class BubbleTest {
 	public static void main(String[] args) {
 		Integer[] arr = {4, 5, 6, 3, 2, 1};
