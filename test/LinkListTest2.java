153c56c2f5055cf74cdfaf0efb923c21eeab02e1
yoyling <2221999792@qq.com>
2021-02-05 23:59:00 +0800

    单链表反转测试

--- test/LinkListTest2.java
+++ test/LinkListTest2.java
@@ -5,25 +5,25 @@ import linear.LinkList;
 
 public class LinkListTest2 {
 
-    public static void main(String[] args) {
-        //创建单向链表对象
-        LinkList<String> sl = new LinkList<>();
-        //测试插入
-        sl.insert("姚明");
-        sl.insert("科比");
-        sl.insert("麦迪");
-        sl.insert(1,"詹姆斯");
+	public static void main(String[] args) {
+		//创建单向链表对象
+		LinkList<String> sl = new LinkList<>();
+		//测试插入
+		sl.insert("姚明");
+		sl.insert("科比");
+		sl.insert("麦迪");
+		sl.insert(1, "詹姆斯");
 
-        for (String s : sl) {
-            System.out.println(s);
-        }
+		for (String s : sl) {
+			System.out.println(s);
+		}
 
-        System.out.println("------------------------------------------");
+		System.out.println("------------------------------------------");
 
-        sl.reverse();
-        for (String s : sl) {
-            System.out.println(s);
-        }
+		sl.reverse();
+		for (String s : sl) {
+			System.out.println(s);
+		}
 
-    }
+	}
 }
