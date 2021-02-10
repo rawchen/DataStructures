20fd9b3c741708a794af551e7c4eb4710362b818
yoyling <2221999792@qq.com>
2021-02-10 23:29:49 +0800

    堆上浮下沉算法

--- heap/Heap.java
+++ heap/Heap.java
@@ -25,18 +25,83 @@ public class Heap<T extends Comparable<T>> {
 
 	//往堆中插入一个元素
 	public void insert(T t) {
+		items[++N] = t;
+		swim(N);
 	}
 
 	//使用上浮算法，使索引k处的元素能在堆中处于一个正确的位置
 	private void swim(int k) {
+		//通过循环，不断的比较当前结点的值和其父结点的值，如果发现父结点的值比当前结点的值小，则交换位置
+		while (k > 1) {
+			//比较当前结点和其父结点
+
+			if (less(k / 2, k)) {
+				exch(k / 2, k);
+			}
+
+			k = k / 2;
+		}
 	}
 
 	//删除堆中最大的元素,并返回这个最大元素
 	public T delMax() {
-		return null;
+		T max = items[1];
+
+		//交换索引1处的元素和最大索引处的元素，让完全二叉树中最右侧的元素变为临时根结点
+		exch(1, N);
+		//最大索引处的元素删除掉
+		items[N] = null;
+		//元素个数-1
+		N--;
+		//通过下沉调整堆，让堆重新有序
+		sink(1);
+		return max;
 	}
 
 	//使用下沉算法，使索引k处的元素能在堆中处于一个正确的位置
 	private void sink(int k) {
+		//通过循环不断的对比当前k结点和其左子结点2*k以及右子结点2k+1处中的较大值的元素大小，如果当前结点小，则需要交换位置
+		while (2 * k <= N) {
+			//获取当前结点的子结点中的较大结点
+			int max;//记录较大结点所在的索引
+			if (2 * k + 1 <= N) {
+				if (less(2 * k, 2 * k + 1)) {
+					max = 2 * k + 1;
+				} else {
+					max = 2 * k;
+				}
+			} else {
+				max = 2 * k;
+			}
+
+			//比较当前结点和较大结点的值
+			if (!less(k, max)) {
+				break;
+			}
+
+			//交换k索引处的值和max索引处的值
+			exch(k, max);
+
+			//变换k的值
+			k = max;
+
+		}
+	}
+
+	public static void main(String[] args) {
+		Heap<String> heap = new Heap<String>(20);
+		heap.insert("A");
+		heap.insert("B");
+		heap.insert("C");
+		heap.insert("D");
+		heap.insert("E");
+		heap.insert("F");
+		heap.insert("G");
+
+		String del;
+		while ((del = heap.delMax()) != null) {
+			System.out.print(del + ",");
+		}
+
 	}
 }
