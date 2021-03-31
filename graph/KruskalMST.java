b37ee543c1a7fd1e1f602976a40f066944044f3d
yoyling <2221999792@qq.com>
2021-03-31 23:45:38 +0800

    最小生成树-Kruskal算法

--- graph/KruskalMST.java
+++ graph/KruskalMST.java
@@ -15,10 +15,42 @@ public class KruskalMST {
 	//根据一副加权无向图，创建最小生成树计算对象
 	public KruskalMST(EdgeWeightedGraph G) {
 
+		//初始化mst
+		this.mst = new Queue<Edge>();
+		//初始化uf
+		this.uf = new UF_Tree_Weighted(G.V());
+		//初始化pq
+		this.pq = new MinPriorityQueue<>(G.E() + 1);
+		//把图中所有的边存储到pq中
+		for (Edge e : G.edges()) {
+			pq.insert(e);
+		}
+
+		//遍历pq队列，拿到最小权重的边，进行处理
+
+		while (!pq.isEmpty() && mst.size() < G.V() - 1) {
+			//找到权重最小的边
+			Edge e = pq.delMin();
+			//找到该边的两个顶点
+			int v = e.either();
+			int w = e.other(v);
+
+			//判断这两个顶点是否已经在同一颗树中，如果在同一颗树中，则不对该边做处理，如果不在一棵树中，则让这两个顶点属于的两棵树合并成一棵树
+			if (uf.connected(v, w)) {
+				continue;
+			}
+
+			uf.union(v, w);
+
+			//让边e进入到mst队列中
+			mst.enqueue(e);
+
+		}
+
 	}
 
 	//获取最小生成树的所有边
 	public Queue<Edge> edges() {
-		return null;
+		return mst;
 	}
 }
